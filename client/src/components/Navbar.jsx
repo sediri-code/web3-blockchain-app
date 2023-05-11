2cd5376f5e539ddb2ac391effd48f77b211a688c
sediri-code <63328434+sediri-code@users.noreply.github.com>
2023-05-11 14:26:48 +0200

    fixx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import logo from '../../images/logo.png';
 const Navbar = () => {
     return (
       <nav className = "w-full flex md:justify-center justify-between items-center p-4"></nav>
-    );
+      
+      );
 }
 export default Navbar;
